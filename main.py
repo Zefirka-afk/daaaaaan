760a75a738b754d8241fe18170d9048b5eabbb93
Zefirka-afk <74975124+Zefirka-afk@users.noreply.github.com>
2025-09-28 22:26:10 +0400

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import telebot
 # =========                   КОНФИГУРАЦИЯ                    =========
 # ===================================================================
 # !!! ВАЖНО: Вставьте сюда свой токен, полученный от @BotFather
-TOKEN = "8183205134:AAEJ95MtbBfYQXOej4ZBxb3GRyS1oz56qlY" 
+TOKEN = "8369181511:AAEvwUn5gQUAXizdvvUpbP4repqU26iKQd0" 
 
 # !!! ВАЖНО: Вставьте сюда основной URL вашего приложения с Render
 # Пример: "https://your-app-name.onrender.com"
@@ -174,3 +174,4 @@ if __name__ == "__main__":
     # Запускаем веб-сервер Flask
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
