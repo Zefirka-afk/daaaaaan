fc19ed04fa6a304e5b019a290ae1ac4b8414b10b
Zefirka-afk <74975124+Zefirka-afk@users.noreply.github.com>
2025-09-29 15:40:36 +0400

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import telebot
 # =========                   КОНФИГУРАЦИЯ                    =========
 # ===================================================================
 # !!! ВАЖНО: Вставьте сюда свой токен, полученный от @BotFather
-TOKEN = "8369181511:AAFVk4TtFaoqx9-K1CUkf901P5xGAMcmMDA" 
+TOKEN = "8441945670:AAE9nQ1pa3vU-oiLbuOTX714fPQnvuXqGcY" 
 
 # !!! ВАЖНО: Вставьте сюда основной URL вашего приложения с Render
 # Пример: "https://your-app-name.onrender.com"
@@ -155,6 +155,7 @@ if __name__ == "__main__":
     
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
